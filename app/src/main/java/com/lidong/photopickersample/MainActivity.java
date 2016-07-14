632375b5b90538bd8a10e2e8213eb28c217ef9af
ziyeyouhu <41492367@qq.com>
2016-07-14 14:16:07 +0800

    Initial commit

---
+++ app/src/main/java/com/lidong/photopickersample/MainActivity.java
@@ -0,0 +1,233 @@
+package com.lidong.photopickersample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.lidong.photopicker.ImageCaptureManager;
+import com.lidong.photopicker.PhotoPickerActivity;
+import com.lidong.photopicker.PhotoPreviewActivity;
+import com.lidong.photopicker.SelectModel;
+import com.lidong.photopicker.intent.PhotoPickerIntent;
+import com.lidong.photopicker.intent.PhotoPreviewIntent;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+/**
+ * @author
+ * @
+ * @date 02-29
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_CODE = 10;
+    private static final int REQUEST_PREVIEW_CODE = 20;
+    private ArrayList<String> imagePaths = new ArrayList<>();
+    private ImageCaptureManager captureManager; // 相机拍照处理类
+
+    private GridView gridView;
+    private GridAdapter gridAdapter;
+    private Button mButton;
+    private String depp;
+    private EditText textView;
+    private String TAG = MainActivity.class.getSimpleName();
+
+
+
+
+    private TextView t1;
+    private TextView t2;
+    private TextView t3;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.gridView);
+        mButton = (Button) findViewById(R.id.button);
+        textView = (EditText) findViewById(R.id.et_context);
+
+
+
+
+
+        t1 = (TextView) findViewById(R.id.t1);
+        t2 = (TextView) findViewById(R.id.t2);
+        t3 = (TextView) findViewById(R.id.t3);
+
+        t1.setText(AlignedTextUtils.formatText("手的机号"));
+        t2.setText(AlignedTextUtils.formatText("密　　码"));
+        t3.setText("再次确认密码");
+//        tv4.setText(AlignedTextUtils.formatText("个性签名"));
+
+
+        int cols = getResources().getDisplayMetrics().widthPixels / getResources().getDisplayMetrics().densityDpi;
+        cols = cols < 4 ? 4 : cols;
+        gridView.setNumColumns(cols);
+
+        // preview
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String imgs = (String) parent.getItemAtPosition(position);
+
+
+                if ("000000".equals(imgs)) {
+                    PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                    intent.setSelectModel(SelectModel.MULTI);
+                    intent.setShowCarema(true); // 是否显示拍照
+                    intent.setMaxTotal(8); // 最多选择照片数量，默认为6
+                    intent.setSelectedPaths(imagePaths); // 已选中的照片地址， 用于回显选中状态
+                    startActivityForResult(intent, REQUEST_CAMERA_CODE);
+                } else {
+                    PhotoPreviewIntent intent = new PhotoPreviewIntent(MainActivity.this);
+                    intent.setCurrentItem(position);
+                    imagePaths.remove(imagePaths.size() - 1);               //去掉最后一个
+                    intent.setPhotoPaths(imagePaths);
+                    startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+                }
+            }
+        });
+        imagePaths.add("000000");
+        gridAdapter = new GridAdapter(imagePaths);
+        gridView.setAdapter(gridAdapter);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                depp = textView.getText().toString().trim() != null ? textView.getText().toString().trim() : "woowoeo";
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+//                        FileUploadManager.uploadMany(imagePaths, depp);
+//                        FileUploadManager.upload(imagePaths,depp);
+
+
+                        Log.i(TAG, "run: "+imagePaths.toString());
+                        Log.i(TAG, "run: "+imagePaths.size());
+                    }
+                }.start();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                // 选择照片
+                case REQUEST_CAMERA_CODE:
+                    ArrayList<String> list = data.getStringArrayListExtra(PhotoPickerActivity.EXTRA_RESULT);
+                    Log.d(TAG, "list: " + "list = [" + list.size());
+                    loadAdpater(list);
+                    break;
+                // 预览
+                case REQUEST_PREVIEW_CODE:
+                    ArrayList<String> ListExtra = data.getStringArrayListExtra(PhotoPreviewActivity.EXTRA_RESULT);
+                    Log.d(TAG, "ListExtra: " + "ListExtra = [" + ListExtra.size());
+                    loadAdpater(ListExtra);
+                    break;
+            }
+        }
+    }
+
+    private void loadAdpater(ArrayList<String> paths) {
+        if (imagePaths != null && imagePaths.size() > 0) {
+            imagePaths.clear();
+        }
+        if (paths.contains("000000")) {
+            paths.remove("000000");
+        }
+        paths.add("000000");
+        imagePaths.addAll(paths);
+        gridAdapter = new GridAdapter(imagePaths);
+        gridView.setAdapter(gridAdapter);
+        try {
+            JSONArray obj = new JSONArray(imagePaths);
+            Log.e("--", obj.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class GridAdapter extends BaseAdapter {
+        private ArrayList<String> listUrls;
+        private LayoutInflater inflater;
+
+        public GridAdapter(ArrayList<String> listUrls) {
+            this.listUrls = listUrls;
+            if (listUrls.size() == 10) {
+                listUrls.remove(listUrls.size() - 1);
+            }
+            inflater = LayoutInflater.from(MainActivity.this);
+        }
+
+        public int getCount() {
+            return listUrls.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return listUrls.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.item_image, parent, false);
+                holder.image = (ImageView) convertView.findViewById(R.id.imageView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            final String path = listUrls.get(position);
+            if (path.equals("000000")) {
+                holder.image.setImageResource(R.mipmap.add_image);
+            } else {
+                Glide.with(MainActivity.this)
+                        .load(path)
+                        .placeholder(R.mipmap.default_error)
+                        .error(R.mipmap.default_error)
+                        .centerCrop()
+                        .crossFade()
+                        .into(holder.image);
+            }
+            return convertView;
+        }
+
+        class ViewHolder {
+            ImageView image;
+        }
+    }
+}
---
+++ app/src/main/java/com/lidong/photopickersample/MyAPP.java
@@ -0,0 +1,22 @@
+package com.lidong.photopickersample;
+
+import android.app.Application;
+
+/**
+ * User: ZiYeYouHu
+ * Date: 2016-07-08
+ * Time: 17:05
+ * Des:
+ * FIXME
+ */
+
+public class MyAPP extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //这里必须在super.onCreate方法之后，顺序不能变
+
+    }
+
+
+}
